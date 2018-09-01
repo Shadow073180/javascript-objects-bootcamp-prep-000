5ff5871147cd400272f3ca466281cf33073a3705
David masterson <david73180@gmail.com>
2018-09-01 22:19:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = {artist_name: "song title"}
 
 function updatePlaylist(playlist, artistName, songTitle){
+<<<<<<< HEAD
   return Object.assign(playlist, {[artistName]: songTitle})
   
 }
@@ -8,4 +9,12 @@ function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName]
   return playlist
   
+=======
+  playlist.artistName = 'songTitle'
+  return playlist
+}
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName
+  return playlist
+>>>>>>> ead72d531c153380489c081d4a6d7707924f931a
 }
